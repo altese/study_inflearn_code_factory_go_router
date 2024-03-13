fce5a4626dc8f26107dc1bd0436ad080e0afece8
altese <readme42u@gmail.com>
2024-03-13 21:59:57 +0900

    🔨 [Push vs Go]
    - push: 뒤로 가기 실행 시 이전 페이지로 이동
    - go: 뒤로 가기 실행 시 상위 페이지로 이동

--- lib/route/router.dart
+++ lib/route/router.dart
@@ -1,27 +1,33 @@
 import 'package:go_router/go_router.dart';
 import 'package:study_inflearn_code_factory_go_router/screens/1_basic_screen.dart';
 import 'package:study_inflearn_code_factory_go_router/screens/2_named_screen.dart';
+import 'package:study_inflearn_code_factory_go_router/screens/3_push_screen.dart';
 import 'package:study_inflearn_code_factory_go_router/screens/root_screen.dart';
 
 final router = GoRouter(
   routes: [
-    // [ / ] -> RootScreen
+    // [ / ] => RootScreen
     GoRoute(
       path: '/',
       builder: (context, state) => const RootScreen(),
       routes: [
-        // [ /basic ] -> BasicScreen
+        // [ /basic ] => BasicScreen
         // path로 이동
         GoRoute(
           path: 'basic',
           builder: (context, state) => const BasicScreen(),
         ),
-        // [ /named ] -> NamedScreen
+        // [ /named ] => NamedScreen
         // name으로 이동
         GoRoute(
           path: 'named',
           name: 'named_screen',
           builder: (context, state) => const NamedScreen(),
+        ),
+        // [ /push ] => PushScreen
+        GoRoute(
+          path: 'push',
+          builder: (context, state) => const PushScreen(),
         ),
       ],
     ),
--- lib/screens/3_push_screen.dart
+++ lib/screens/3_push_screen.dart
@@ -2,31 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:study_inflearn_code_factory_go_router/layout/default_layout.dart';
 
-class RootScreen extends StatelessWidget {
-  const RootScreen({super.key});
+class PushScreen extends StatelessWidget {
+  const PushScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return DefaultLayout(
       body: ListView(
         children: [
+          // 뒤로 가기 => 이전 페이지
+          ElevatedButton(
+            onPressed: () {
+              context.push('/basic');
+            },
+            child: const Text('Push Screen'),
+          ),
+          // 뒤로 가기 => '/'
           ElevatedButton(
             onPressed: () {
               context.go('/basic');
             },
             child: const Text('Go Basic'),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              context.goNamed('named_screen');
-            },
-            child: const Text('Go Named'),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              context.go('/push');
-            },
-            child: const Text('Go Push'),
           ),
         ],
       ),
